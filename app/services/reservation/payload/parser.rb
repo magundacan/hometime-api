db187e12f5711a81ea89683073b689e88281ec04
magundacan <magundacan.software@gmail.com>
2023-08-29 02:49:03 +0800

    Complete the Reservation::Payload::Parser class definition

--- app/services/reservation/payload/parser.rb
+++ app/services/reservation/payload/parser.rb
@@ -1,2 +1,25 @@
 class Reservation::Payload::Parser < ApplicationService
+  attr_reader :params
+
+  def initialize(params)
+    @params = params
+  end
+
+  def call
+    {
+      code: CodeParser.call(params),
+      status: StatusParser.call(params),
+      start_date: StartDateParser.call(params),
+      end_date: EndDateParser.call(params),
+      number_of_nights: NumberOfNightsParser.call(params),
+      number_of_guests: NumberOfGuestsParser.call(params),
+      number_of_adults: NumberOfAdultsParser.call(params),
+      number_of_children: NumberOfChildrenParser.call(params),
+      number_of_infants: NumberOfInfantsParser.call(params),
+      total_price_in_cents: TotalPriceInCentsParser.call(params),
+      payout_price_in_cents: PayoutPriceInCentsParser.call(params),
+      security_price_in_cents: SecurityPriceInCentsParser.call(params),
+      currency: CurrencyParser.call(params)
+    }
+  end
 end
