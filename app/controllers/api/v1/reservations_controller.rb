e7d30a19747392bd0684ed4fb40c6e759c0978c6
magundacan <magundacan.software@gmail.com>
2023-08-28 23:39:12 +0800

    Add Reservation model methods

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::ReservationsController < ApplicationController
   def create
-    reservation = Reservation.new(reservation_params)
+    reservation = Reservation.build_with_guest(reservation_params, guest_params)
 
-    if reservation.save
-      render json: reservation, status: :created
+    if reservation.save_with_guest
+      render json: reservation, status: :ok
     else
       render json: { errors: reservation.errors }, status: :unprocessable_entity
     end
@@ -12,5 +12,8 @@ class Api::V1::ReservationsController < ApplicationController
   private
 
   def reservation_params
+  end
+
+  def guest_params
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,24 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
+
+  def self.build_with_guest(reservation_attributes, guest_attributes)
+    reservation = Reservation.find_or_initialize_by(code: reservation_attributes[:code])
+    guest       = Guest.find_or_initialize_by(email: guest_attributes[:email])
+
+    reservation.assign_attributes(reservation_attributes)
+    guest.assign_attributes(guest_attributes)
+
+    reservation.guest = guest
+    reservation
+  end
+
+  def save_with_guest
+    transaction do
+      save!
+      guest.save!
+    end
+    true
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
 end
