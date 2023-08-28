9edfb7ac45dbea02715d435e37b82a10d93d3683
magundacan <magundacan.software@gmail.com>
2023-08-28 19:03:45 +0800

    Run the recently created migrations

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_104344) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "guests", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
+    t.string "number"
+    t.bigint "guest_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["guest_id"], name: "index_phone_numbers_on_guest_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "code"
+    t.string "status"
+    t.bigint "guest_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "number_of_nights"
+    t.integer "number_of_guests"
+    t.integer "number_of_adults"
+    t.integer "number_of_children"
+    t.integer "number_of_infants"
+    t.integer "total_price_in_cents"
+    t.integer "payout_price_in_cents"
+    t.integer "security_price_in_cents"
+    t.string "currency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["guest_id"], name: "index_reservations_on_guest_id"
+  end
+
+end
