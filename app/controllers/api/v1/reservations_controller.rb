24f3a64f61e4d5ee2baf4b8901053a63bb96871d
magundacan <magundacan.software@gmail.com>
2023-08-28 19:47:12 +0800

    Initial controller structure

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::ReservationsController < ApplicationController
   def create
+    reservation = Reservation.new(reservation_params)
+
+    if reservation.save
+      render json: reservation, status: :created
+    else
+      render json: { errors: reservation.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
   end
 end
