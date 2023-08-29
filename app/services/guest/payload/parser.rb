e9f0c0ab1b8f61f4596a03d5a97c41ad57321a5c
magundacan <magundacan.software@gmail.com>
2023-08-30 03:49:56 +0800

    Fix namespacing

--- app/services/guest/payload/parser.rb
+++ app/services/guest/payload/parser.rb
@@ -1,15 +1,17 @@
-class Guest::Payload::Parser < ApplicationService
-  attr_reader :params
-
-  def initialize(params)
-    @params = params
-  end
-
-  def call
-    {
-      email: EmailParser.call(params),
-      first_name: FirstNameParser.call(params),
-      last_name: LastNameParser.call(params)
-    }
+module Guest::Payload
+  class Parser < ApplicationService
+    attr_reader :params
+  
+    def initialize(params)
+      @params = params
+    end
+  
+    def call
+      {
+        email: EmailParser.call(params),
+        first_name: FirstNameParser.call(params),
+        last_name: LastNameParser.call(params)
+      }
+    end
   end
 end
--- app/services/reservation/payload/parser.rb
+++ app/services/reservation/payload/parser.rb
@@ -1,25 +1,27 @@
-class Reservation::Payload::Parser < ApplicationService
-  attr_reader :params
-
-  def initialize(params)
-    @params = params
-  end
-
-  def call
-    {
-      code: CodeParser.call(params),
-      status: StatusParser.call(params),
-      start_date: StartDateParser.call(params),
-      end_date: EndDateParser.call(params),
-      number_of_nights: NumberOfNightsParser.call(params),
-      number_of_guests: NumberOfGuestsParser.call(params),
-      number_of_adults: NumberOfAdultsParser.call(params),
-      number_of_children: NumberOfChildrenParser.call(params),
-      number_of_infants: NumberOfInfantsParser.call(params),
-      total_price_in_cents: TotalPriceInCentsParser.call(params),
-      payout_price_in_cents: PayoutPriceInCentsParser.call(params),
-      security_price_in_cents: SecurityPriceInCentsParser.call(params),
-      currency: CurrencyParser.call(params)
-    }
+module Reservation::Payload
+  class Parser < ApplicationService
+    attr_reader :params
+  
+    def initialize(params)
+      @params = params
+    end
+  
+    def call
+      {
+        code: CodeParser.call(params),
+        status: StatusParser.call(params),
+        start_date: StartDateParser.call(params),
+        end_date: EndDateParser.call(params),
+        number_of_nights: NumberOfNightsParser.call(params),
+        number_of_guests: NumberOfGuestsParser.call(params),
+        number_of_adults: NumberOfAdultsParser.call(params),
+        number_of_children: NumberOfChildrenParser.call(params),
+        number_of_infants: NumberOfInfantsParser.call(params),
+        total_price_in_cents: TotalPriceInCentsParser.call(params),
+        payout_price_in_cents: PayoutPriceInCentsParser.call(params),
+        security_price_in_cents: SecurityPriceInCentsParser.call(params),
+        currency: CurrencyParser.call(params)
+      }
+    end
   end
 end
