a93db1dc007acd7224af6707d1e3e02640f367c9
magundacan <magundacan.software@gmail.com>
2023-08-31 02:39:29 +0800

    Add some request specs

---
+++ spec/requests/airbnb_reservations_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "AirbnbReservations", type: :request do
+  describe "POST /api/v1/reservations" do
+    context "with valid parameters" do
+      let(:valid_params) do
+        {
+          "reservation_code": "YYY12345678",
+          "start_date": "2021-04-14",
+          "end_date": "2021-04-18",
+          "nights": 4,
+          "guests": 4,
+          "adults": 2,
+          "children": 2,
+          "infants": 0,
+          "status": "accepted",
+          "guest": {
+            "first_name": "Wayne",
+            "last_name": "Woodbridge",
+            "phone": "639123456789",
+            "email": "wayne_woodbridge@bnb.com"
+          },
+          "currency": "AUD",
+          "payout_price": "4200.00",
+          "security_price": "500",
+          "total_price": "4700.00"
+        }
+      end
+
+      it "creates a new reservation under a new guest" do
+        expect { post "/api/v1/reservations", params: valid_params }.to change { Reservation, :count }.by(+1).and \
+                                                                        change { Guest,       :count }.by(+1)
+
+        json_response = JSON.parse(response.body).deep_symbolize_keys
+        expect(json_response[:data][:attributes][:code]).to eq(valid_params[:reservation_code])
+        expect(json_response[:include][:attributes][:email]).to eq(valid_params[:guest][:email])
+
+        expect(response).to have_http_status :ok
+      end
+    end
+  end
+end
---
+++ spec/requests/booking_reservations_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "BookingReservations", type: :request do
+  describe "POST /api/v1/reservations" do
+    context "with valid parameters" do
+      let(:valid_params) do
+        {
+          "reservation": {
+            "code": "XXX12345678",
+            "start_date": "2021-03-12",
+            "end_date": "2021-03-16",
+            "expected_payout_amount": "3800.00",
+            "guest_details": {
+              "localized_description": "4 guests",
+              "number_of_adults": 2,
+              "number_of_children": 2,
+              "number_of_infants": 0
+            },
+            "guest_email": "wayne_woodbridge@bnb.com",
+            "guest_first_name": "Wayne",
+            "guest_last_name": "Woodbridge",
+            "guest_phone_numbers": [
+              "639123456789",
+              "639123456789"
+            ],
+            "listing_security_price_accurate": "500.00",
+            "host_currency": "AUD",
+            "nights": 4,
+            "number_of_guests": 4,
+            "status_type": "accepted",
+            "total_paid_amount_accurate": "4300.00"
+          }
+        }
+      end
+
+      it "creates a new reservation under a new guest" do
+        expect { post "/api/v1/reservations", params: valid_params }.to change { Reservation, :count }.by(+1).and \
+                                                                        change { Guest,       :count }.by(+1)
+
+        json_response = JSON.parse(response.body).deep_symbolize_keys
+        expect(json_response[:data][:attributes][:code]).to eq(valid_params[:reservation][:code])
+        expect(json_response[:include][:attributes][:email]).to eq(valid_params[:reservation][:guest_email])
+
+        expect(response).to have_http_status :ok
+      end
+    end
+  end
+end
